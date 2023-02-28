07b55de756330bde802bcd643d3abff859bb6e35
jeva332 <99867256+jeva332@users.noreply.github.com>
2023-02-28 09:11:06 +0200

    v0.1 arrays

---
+++ Header.h
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::left;
+using std::right;
+using std::setw;
+using std::setprecision;
+using std::string;
+using std::fixed;
+
+
+
+
